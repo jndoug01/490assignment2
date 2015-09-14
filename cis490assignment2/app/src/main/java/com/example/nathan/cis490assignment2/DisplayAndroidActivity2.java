716ba4e1e75129185ac5d052d99a6b92bcd31810
jndoug01 <jndoug01@louisville.edu>
2015-09-14 16:25:44 -0400

    initial commit

---
+++ cis490assignment2/app/src/main/java/com/example/nathan/cis490assignment2/DisplayAndroidActivity2.java
@@ -0,0 +1,44 @@
+package com.example.nathan.cis490assignment2;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class DisplayAndroidActivity2 extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_android_activity2);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_display_android_activity2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void displayLogo(View view) {
+        Intent intent = new Intent(this, DisplayAndroidActivity2.class);
+    }
+}
